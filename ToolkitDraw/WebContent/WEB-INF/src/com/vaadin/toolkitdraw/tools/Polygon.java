cb3fa74768df454d1cf8661d2190665427867df5
john.ahlroos@itmill.com <john.ahlroos@itmill.com@de684a11-831f-0410-ab27-cb86445eb682>
2009-05-29 13:12:21 +0000

    Added basic tools for the polygon tool
    git-svn-id: http://dev.vaadin.com/svn/incubator/ToolkitDraw@8103 de684a11-831f-0410-ab27-cb86445eb682

--- ToolkitDraw/WebContent/WEB-INF/src/com/vaadin/toolkitdraw/tools/Polygon.java
+++ ToolkitDraw/WebContent/WEB-INF/src/com/vaadin/toolkitdraw/tools/Polygon.java
@@ -7,14 +7,43 @@ import com.vaadin.toolkitdraw.tools.Tool.Type;
 import com.vaadin.toolkitdraw.util.IconFactory;
 import com.vaadin.toolkitdraw.util.IconFactory.Icons;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Layout;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
 public class Polygon extends Tool implements ValueChangeListener {
 
+	private TextField size;
+	
+	private TextField color;
+	
+	private TextField fillColor;
+	
+	private Layout layout = new VerticalLayout();
+	
 	public Polygon(PaintCanvas canvas){
 		this.canvas = canvas;		
 		button = new Button();
 		button.setData(Type.POLYGON);
 		button.setIcon(IconFactory.getIcon(Icons.ICON_POLY));
+			
+		size = new TextField("Size");
+		size.addListener(this);
+		size.setImmediate(true);
+		size.setValue(1);
+		layout.addComponent(size);		
+		
+		color = new TextField("Color");
+		color.addListener(this);
+		color.setImmediate(true);
+		color.setValue("000000");
+		layout.addComponent(color);
+		
+		fillColor = new TextField("Fill Color");
+		fillColor.addListener(this);
+		fillColor.setImmediate(true);
+		fillColor.setValue("");
+		layout.addComponent(fillColor);
 	}
 	
 	@Override
@@ -22,12 +51,23 @@ public class Polygon extends Tool implements ValueChangeListener {
 		return Type.POLYGON;
 	}	
 	
-		
+	public Layout createToolOptions(){	
+		return layout;
+	}		
 	
 	@Override
 	public void valueChange(ValueChangeEvent event) {
-		// TODO Auto-generated method stub
-
+		if(canvas == null) return;			
+		
+		if(event.getProperty() == size){							
+			canvas.setToolSize(Double.parseDouble(event.getProperty().getValue().toString()));				
+		}
+		else if(event.getProperty() == color){
+			canvas.setColor(String.valueOf(event.getProperty().getValue()));
+		}		
+		else if(event.getProperty() == fillColor){
+			canvas.setFillColor(String.valueOf(event.getProperty().getValue()));
+		}
 	}
 
 }
